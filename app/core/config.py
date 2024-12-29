3724f0d81f145f7c2ae206788c648540cde9b525
eunhoo0903 <eunhoo0903@naver.com>
2024-12-29 19:49:42 +0900

    DB 설정

---
+++ app/core/config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+
+if not SECRET_KEY: # 시크릿 키가 없다면면
+    raise ValueError("No SECRET_KEY set for FastAPI application")
